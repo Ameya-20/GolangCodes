9ec77444922bf2af3cc1f47821877dd795f16a7e
Ameya <53653710+Ameya-20@users.noreply.github.com>
2020-06-06 14:35:18 +0530

    Add files via upload

---
+++ TicTacToeGame/go.mod
@@ -0,0 +1,3 @@
+module example/github.com/Ameya-20/GolangCodes/tree/master/TicTacToe_Game
+
+go 1.14
---
+++ TicTacToeGame/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/Ameya-20/GolangCodes/tree/master/TicTacToeGame/tictactoe"
+
+func main() {
+	tictactoe.main()
+}
